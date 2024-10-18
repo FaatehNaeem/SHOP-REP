2f506d0eb1da24e710e940ac12e24ff25f58f619
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-10-18 18:30:59 +0500

    added testimonial-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
 import HeroSection from "./sections/hero-section";
-import ServicesSection from "./sections/services-section";
+import ServicesSection from "@/sections/services-section";
 
 export default function App() {
-
-    return (
-        <div className="flex min-h-screen w-screen flex-col bg-background">
-            <Header />
-            <HeroSection />
-            <ServicesSection/>
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="flex min-h-screen w-screen flex-col bg-background">
+      <Header />
+      <HeroSection />
+      <ServicesSection />
+      <Footer />
+    </div>
+  );
 }
