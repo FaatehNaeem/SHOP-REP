ede30a1a014a5a370e09ce18b672883ae33c6d05
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-10-18 18:17:52 +0500

    services-section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
 import HeroSection from "./sections/hero-section";
+import ServicesSection from "./sections/services-section";
 
 export default function App() {
 
     return (
         <div className="flex min-h-screen w-screen flex-col bg-background">
             <Header />
-            <HeroSection/>
+            <HeroSection />
+            <ServicesSection/>
             <Footer />
         </div>
     )
