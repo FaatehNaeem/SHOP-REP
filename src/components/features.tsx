113640de68900a67bd51eae7cdf99af2841474a9
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-10-18 21:49:33 +0500

    features-section

---
+++ src/components/features.tsx
@@ -0,0 +1,22 @@
+import FeatureCard from "@/components/feature-card";
+
+export default function Features() {
+  return (
+    <div className="w-screen flex flex-col mt-6 justify-center items-center">
+      <FeatureCard
+        heading="Guided Shopping"
+        description=" Seamlessly Connect to Live Agents When It Matters Most"
+      />
+
+      <FeatureCard
+        heading="Guided Shopping"
+        description=" Seamlessly Connect to Live Agents When It Matters Most"
+      />
+
+      <FeatureCard
+        heading="Guided Shopping"
+        description=" Seamlessly Connect to Live Agents When It Matters Most"
+      />
+    </div>
+  );
+}
