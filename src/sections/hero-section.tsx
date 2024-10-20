0c90662beb23bad2e96a8fe0be04fddbf1909e2a
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-10-20 13:39:27 +0500

    responsiveness header-section

--- src/sections/hero-section.tsx
+++ src/sections/hero-section.tsx
@@ -1,11 +1,12 @@
 import LeadingBrandsBanner from "@/components/leading-brands-banner";
 import { Button } from "@/components/ui/button";
-import { CheckCircle2Icon, Star } from "lucide-react";
+import { CheckCircle2Icon } from "lucide-react";
 
 export default function HeroSection() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="h-[187px] text-center mt-2">
+      {/* heading and para */}
+      <div className="text-center mt-2">
         <h1 className="text-primary-foreground font-bold">Boost Sales &</h1>
         <h1 className="text-secondary-foreground font-bold">
           Delight Customers
@@ -14,43 +15,47 @@ export default function HeroSection() {
           Instant responses and seamless multi-channel support
         </p>
       </div>
-      <Button className="bg-secondary-foreground rounded-3xl w-[236px] h-[44px] text-background focus-visible:ring-0 hover:border-none hover:bg-teal-500 focus:outline-none">
+      {/* btn */}
+      <Button className="bg-secondary-foreground rounded-3xl w-[236px] h-[44px] text-background focus-visible:ring-0 hover:border-none hover:bg-teal-500 focus:outline-none mt-2">
         Start Your Free Trial Today
       </Button>
 
-      <div className="flex-col h-[84px] mt-8">
-        <div className="flex flex-row gap-4">
-          <CheckCircle2Icon className="text-secondary-foreground" />
-          <p className="text-primary-foreground">100% Growth guaranteed</p>
-          <CheckCircle2Icon className="text-secondary-foreground" />
-          <p className="text-primary-foreground">100% Growth guaranteed</p>
-          <CheckCircle2Icon className="text-secondary-foreground" />
-          <p className="text-primary-foreground">100% Growth guaranteed</p>
-        </div>
-
-        <div className="flex flex-row gap-4 mt-4 items-center">
-          <img
-            src="hero-section-icon.png"
-            alt=""
-            width={"116px"}
-            height={"32px"}
-          />
-          <p className="text-zinc-500">Trusted by 1230+ businesses</p>
-          <div className="border border-zinc-300 h-6"></div>
-          <p className="text-primary-foreground font-bold">4.9</p>
-          <div className="flex flex-row items-center">
-            <Star color="yellow" fill="yellow" />
-            <Star color="yellow" fill="yellow" />
-            <Star color="yellow" fill="yellow" />
-            <Star color="yellow" fill="yellow" />
-            <Star color="yellow" fill="yellow" />
-            <p className="text-zinc-500 ml-2">from 10k+ reviews</p>
+      <div className="flex-col mt-8">
+        {/* 100% growth section */}
+        <div className="flex flex-row gap-4 growth-guaranteed-section">
+          <div className="flex flex-row">
+            <CheckCircle2Icon className="text-secondary-foreground" />
+            <p className="text-primary-foreground ml-2">
+              100% Growth guaranteed
+            </p>
+          </div>
+          <div className="flex flex-row">
+            <CheckCircle2Icon className="text-secondary-foreground" />
+            <p className="text-primary-foreground ml-2">
+              100% Growth guaranteed
+            </p>
+          </div>
+          <div className="flex flex-row">
+            <CheckCircle2Icon className="text-secondary-foreground" />
+            <p className="text-primary-foreground ml-2">
+              100% Growth guaranteed
+            </p>
           </div>
         </div>
+
+        {/* rating */}
+        <div className="flex flex-row gap-4 mt-4 items-center ratings-container">
+          <img src="trusted-by-1230.svg" alt="" className="trusted-icon" />
+          <div className="border border-zinc-300 h-6 ratings-divider"></div>
+
+          <img src="rating.svg" alt="" className="rating-icon" />
+        </div>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      {/* ui ss */}
+      <div className="flex flex-col justify-center items-center mt-6">
         <img src="hero-section-img.png" alt="" />
       </div>
+      {/* banner */}
       <LeadingBrandsBanner />
     </div>
   );
