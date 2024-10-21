735ae6aa49392ef53f9693ca90c1f346ef229f12
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-10-21 23:27:08 +0500

    questions-section-2

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,8 +1,10 @@
 import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
 import CallToActionBanner from "@/components/cta-banner";
+import LeadingBrandsBanner from "@/components/leading-brands-banner";
 import HeroSection from "@/sections/pricing/hero-section";
 import QuestionSection from "@/sections/pricing/question-section";
+import QuestionSection2 from "@/sections/pricing/question-section-2";
 import TestimonialSection from "@/sections/pricing/testimonial-section";
 
 export default function Pricing() {
@@ -16,6 +18,8 @@ export default function Pricing() {
         marginTop="0px"
       />
       <QuestionSection />
+      <QuestionSection2 />
+      <LeadingBrandsBanner/>
       <Footer />
     </div>
   );
