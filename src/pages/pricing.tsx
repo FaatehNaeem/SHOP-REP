e7f6ad903c9164621b0eebbc016a88c6931e00e1
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-10-21 13:44:11 +0500

    added sections for pages

---
+++ src/pages/pricing.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/common/header";
+import HeroSection from "@/sections/pricing/hero-section";
+
+export default function Pricing() {
+    return (
+        <div className="flex w-screen flex-col bg-background">
+            <Header />
+            <HeroSection/>
+        </div>
+    )
+}
+
